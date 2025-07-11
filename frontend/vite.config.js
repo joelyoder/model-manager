11e8582e3b4cab07f61a57cbf1d25d55779390a4
Joel Yoder <github@joelyoder.com>
2025-07-11 02:31:11 -0400

    Proxy image path during dev

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -17,7 +17,8 @@ export default defineConfig({
   },
   server: {
     proxy: {
-      '/api': 'http://localhost:8080'
+      '/api': 'http://localhost:8080',
+      '/images': 'http://localhost:8080'
     }
   },
 })
