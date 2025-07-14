65a89e1f7e8d6621fc94a6a7e2bac112a8bbd5ad
Joel Yoder <github@joelyoder.com>
2025-07-13 22:59:55 -0400

    Add UI enhancements and download progress

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,6 +2,27 @@
   <div class="min-vh-100">
     <h1 class="p-4 h2 text-center">Model Manager</h1>
     <router-view />
+    <div
+      id="toast-container"
+      class="toast-container position-fixed bottom-0 end-0 p-3"
+    ></div>
+    <div class="modal fade" id="confirm-modal" tabindex="-1">
+      <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+          <div class="modal-body">Are you sure?</div>
+          <div class="modal-footer">
+            <button
+              type="button"
+              class="btn btn-secondary"
+              data-bs-dismiss="modal"
+            >
+              Cancel
+            </button>
+            <button type="button" class="btn btn-primary">OK</button>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
