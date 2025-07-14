3fc124d9828762e07a0503dc882d521ceb8fb419
Joel Yoder <github@joelyoder.com>
2025-07-14 16:06:24 -0400

    Fix gallery image paths on Windows

--- frontend/src/components/ModelDetail.vue
+++ frontend/src/components/ModelDetail.vue
@@ -189,7 +189,7 @@ const galleryImages = computed(() => {
   const imgs = version.value.images || [];
   return imgs.map((img) => ({
     ...img,
-    url: img.path.replace(/^.*\/backend\/images/, "/images"),
+    url: img.path.replace(/^.*[\\/]backend[\\/]images/, "/images"),
     parsedMeta: parseMeta(img.meta),
   }));
 });
